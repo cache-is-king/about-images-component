f355b7a5dc704f855a6bed502367ad591f214ae0
kevinshare <kevman9@gmail.com>
2018-03-19 16:46:53 -0700

    comment tests

--- spec/server.test.js
+++ spec/server.test.js
@@ -4,7 +4,7 @@ const app = require('../server/app.js');
 
 
 xdescribe('should send back a 200 if static file is found', () => {
-  test('It should respond with a 200', (done) => {
+  xtest('It should respond with a 200', (done) => {
     request(app).get('/restaurants/90976').then((response) => {
       expect(response.statusCode).toBe(200);
       done();
@@ -13,7 +13,7 @@ xdescribe('should send back a 200 if static file is found', () => {
 });
 
 xdescribe('should receive a 404 if route isnt correct', () => {
-  test('should receive a 404 from bad url', (done) => {
+  xtest('should receive a 404 from bad url', (done) => {
     request(app).get('/gibberish').then((response) => {
       expect(response.statusCode).toBe(404);
       done();
@@ -22,7 +22,7 @@ xdescribe('should receive a 404 if route isnt correct', () => {
 });
 
 xdescribe('GET route to /restaurants/:id/ should return 200 status', () => {
-  test('should receive 200 status code', (done) => {
+  xtest('should receive 200 status code', (done) => {
     request(app).get('/restaurants/90976').then((response) => {
       expect(response.statusCode).toBe(200);
       done();
@@ -31,7 +31,7 @@ xdescribe('GET route to /restaurants/:id/ should return 200 status', () => {
 });
 
 xdescribe('GET route to /restaurants/:id', () => {
-  test('should receive appropriate body', (done) => {
+  xtest('should receive appropriate body', (done) => {
     request(app).get('/restaurants/89104').then((response) => {
       expect(response.body[0].id).toBe(89104);
       expect(response.body[0].name).toBe('Tokyo Katana-Ya');
